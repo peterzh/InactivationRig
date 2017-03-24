2bc7fb3b411059d2c7cbc021f05198fbcbd6a7f7
Peter Zatka-Haas <p.zatka@ucl.ac.uk>
2017-03-24 16:57:10 +0000

    live video feed update + added calibration for stereotaxic coords

--- ThorCam.m
+++ ThorCam.m
@@ -6,12 +6,18 @@ classdef ThorCam < handle
         ImageBits;
         MemID;
         isCapturing;
+        
+        %calibration between pixel identity and real position
         pos2pix_transform;
         pix2pos_transform;
         
+        %calibration between real position and stereotaxic coords
+        pos2ste_transform;
+        ste2pos_transform;
+        
+        %misc items related to live video feed 
         vidAx;
         vidTimer;
-
     end
     
     methods
@@ -43,6 +49,18 @@ classdef ThorCam < handle
             
             %Extract image width/height/bits
             [~,obj.ImageWidth,obj.ImageHeight,obj.ImageBits,~] = obj.camObj.Memory.Inquire(obj.MemID);
+            
+            %Create figure for live video feed
+            figure;
+            axes; colormap(gray);
+            obj.vidAx = gca;
+            obj.isCapturing = 0;
+            obj.vidTimer = timer;
+            obj.vidTimer.TimerFcn=@(tHandle,tEvents)(obj.timercallback);
+            obj.vidTimer.Period = 0.4;
+            obj.vidTimer.TasksToExecute = Inf;
+            obj.vidTimer.ExecutionMode = 'fixedRate';
+            start(obj.vidTimer);
         end
         
         function start(obj)
@@ -52,77 +70,84 @@ classdef ThorCam < handle
             
             [~,fps]=obj.camObj.Timing.Framerate.GetCurrentFps;
             [~,fpsrange]=obj.camObj.Timing.Framerate.GetFrameRateRange;
-            disp(['frames per second: ' num2str(fps) '   min:' num2str(fpsrange.Minimum) ' max:' num2str(fpsrange.Maximum)]);
+            disp(['camera framerate: ' num2str(fps) '   min:' num2str(fpsrange.Minimum) ' max:' num2str(fpsrange.Maximum)]);
             
             
             [~,exposure]=obj.camObj.Timing.Exposure.Get;
             [~,exposurerange]=obj.camObj.Timing.Exposure.GetRange;
             disp(['exposure: ' num2str(exposure) '   min:' num2str(exposurerange.Minimum) ' max:' num2str(exposurerange.Maximum)]);
             
-            
-            %            %create timer object to keep updating the image in the
-            %            %background
-            figure('color','g');
-            axes;
-            obj.vidAx = gca;
-            set(obj.vidAx,'xtick','','ytick','','box','off','xcolor','w','ycolor','w');
-            
-            obj.vidTimer = timer;
-            obj.vidTimer.TimerFcn=@(tHandle,tEvents)(obj.timercallback);
-            obj.vidTimer.Period = 0.4;
-            obj.vidTimer.TasksToExecute = Inf;
-            obj.vidTimer.ExecutionMode = 'fixedRate';
-            start(obj.vidTimer);
-            
         end
 %         
         function stop(obj)
             obj.isCapturing = 0;
-            stop(obj.vidTimer);
             obj.camObj.Acquisition.Stop;
         end
         
         function img = getFrame(obj)
-            if obj.isCapturing == 0
-                obj.start;
-            end
 %             obj.camObj.Acquisition.Freeze(uc480.Defines.DeviceParameter.Wait); 
             [~,tmp] = obj.camObj.Memory.CopyToArray(obj.MemID);
             img = reshape(tmp.uint8,obj.ImageWidth,obj.ImageHeight);
         end
 
-        
         function setGain(obj,index)
             %index = 0-100;
             [~,factor] = obj.camObj.Gain.Hardware.ConvertScaledToFactor.Master(index);
             obj.camObj.Gain.Hardware.Factor.SetMaster(factor)
         end
         
-        function setFps(obj,fps)
-            obj.camObj.Timing.Framerate.Set(fps)
+        function ga = getGain(obj)
+            [~,ga]=obj.camObj.Gain.Hardware.Factor.GetMaster;
+        end
+        
+        function setUpdateRate(obj,fps)
+%             obj.camObj.Timing.Framerate.Set(fps)
+            %Hack by using different timer periods
+            period = 1/fps;
+            stop(obj.vidTimer);
+            obj.vidTimer.Period = period;
+            start(obj.vidTimer);
         end
         
         function setExposure(obj,exposure)
             obj.camObj.Timing.Exposure.Set(exposure)
         end
         
-        function pos = getStimPos(obj) %locate position of dot 
-            %TODO
-            %image processing to find pixel position of peaks, and then use
-            %calibrated transformations to get the position in real space
-            
-            %In the mean time just show image, and click on the location
-            img = obj.getFrame;
-            f=figure;
-            ax = axes('Parent',f);
-            image(img,'Parent',ax);
-            title('Click on centre of dot');
-            [pix_x,pix_y]=ginput(1); 
-            
-            pos = obj.pix2pos([pix_x pix_y]);
+        function ex = getExposure(obj)
+            [~,ex]=obj.camObj.Timing.Exposure.Get;
         end
         
-        function obj = calib(obj)
+        function pos = getStimPos(obj,mode) %locate position of dot 
+            img = obj.getFrame;
+            pix=nan(1,2); 
+            
+            switch(mode)
+                case 'manual'
+                    f=figure;
+                    ax = axes('Parent',f);
+                    image(img,'Parent',ax);
+                    title('Select position');
+                    [pix(1),pix(2)]=ginput(1);
+                    close(f);
+
+                case 'auto'
+                    %auto detect peaks
+                    SmImg = imgaussfilt(img,20);
+                    [~,pix(1)]=max(mean(SmImg,1));
+                    [~,pix(1)]=max(mean(SmImg,2));
+                    
+                    f=figure;
+                    ax = axes('Parent',f);
+                    image(img,'Parent',ax); hold on;
+                    h=plot(pix(1),pix(2),'ro'); h.MarkerSize=10;
+                    
+                    pause(2);
+                    close(f);
+            end
+            pos = obj.pix2pos(pix);
+        end
+        
+        function obj = calibPIX2POS(obj)
             %Calib pixel position to real position, requires displaying a
             %grid
             
@@ -162,10 +187,67 @@ classdef ThorCam < handle
             
         end
         
+        function obj = calibPIX2STE(obj)
+            %Calibrate pixel position to stereotaxic coords
+            
+            %get 'real pos' of bregma and lambda, requires PIX2POS calib
+            %already
+            if isempty(obj.pix2pos_transform)
+                error('Need to do pix2pos calibration')
+            end
+            disp('SELECT BREGMA');
+            bregmaPos = obj.getStimPos('manual');
+
+            obj.pos2ste_transform.offset = -bregmaPos; 
+            obj.ste2pos_transform.offset = bregmaPos;
+            
+            disp('SELECT LAMBDA');
+            lambdaPos = obj.getStimPos('manual');
+            
+            delta = bregmaPos-lambdaPos;
+            
+            angle_to_Yaxis = acos( dot(delta,[0 1])/norm(delta) );
+            
+            if delta(1) > 0
+                %clockwise relative to Y axis
+                
+                obj.ste2pos_transform.rotation = [cos(2*pi - angle_to_Yaxis) -sin(2*pi - angle_to_Yaxis);
+                                          sin(2*pi - angle_to_Yaxis) cos(2*pi - angle_to_Yaxis)];
+                                      
+                obj.pos2ste_transform.rotation = [cos(angle_to_Yaxis) -sin(angle_to_Yaxis);
+                                          sin(angle_to_Yaxis) cos(angle_to_Yaxis)];
+                
+            else
+                %anticlockwise relative to Y axis
+                obj.ste2pos_transform.rotation = [cos(angle_to_Yaxis) -sin(angle_to_Yaxis);
+                                          sin(angle_to_Yaxis) cos(angle_to_Yaxis)];
+                                      
+                obj.pos2ste_transform.rotation = [cos(2*pi - angle_to_Yaxis) -sin(2*pi - angle_to_Yaxis);
+                                          sin(2*pi - angle_to_Yaxis) cos(2*pi - angle_to_Yaxis)];
+            end
+            
+            
+            %plot REAL SPACE grid
+            img = obj.getFrame;
+            f=figure;
+            image(img); hold on;
+            
+            dots = -2:0.5:2;
+            [x,y] = meshgrid(dots); x=x(:); y=y(:);
+            pix = obj.pos2pix([x y]);
+            plot(pix(:,1),pix(:,2),'ko');
+          
+            %plot STEREOTAXIC grid
+            pos = bsxfun(@plus,[x,y]*obj.ste2pos_transform.rotation',obj.ste2pos_transform.offset);
+            pix = obj.pos2pix(pos);
+            plot(pix(:,1),pix(:,2),'go');
+            
+        end
+        
         function pos=pix2pos(obj,pix)
             %pix is [nx2]
             if isempty(obj.pix2pos_transform)
-                error('need to calibrate');
+                error('need to calibrate pixel to real space');
             end
             pos = bsxfun(@plus,obj.pix2pos_transform.b * pix * obj.pix2pos_transform.T, obj.pix2pos_transform.c);
         end
@@ -173,20 +255,53 @@ classdef ThorCam < handle
         function pix=pos2pix(obj,pos)
             %pos is [nx2]
             if isempty(obj.pos2pix_transform)
-                error('need to calibrate');
+                error('need to calibrate pixel to real space');
             end
             pix = bsxfun(@plus,obj.pos2pix_transform.b * pos * obj.pos2pix_transform.T, obj.pos2pix_transform.c);
         end
         
-        function timercallback(obj)
-            if obj.isCapturing == 1
-                img = obj.getFrame;
-                image(img,'Parent',obj.vidAx);
+        function pos=ste2pos(obj,ste)
+            %ste is [nx2]
+            if isempty(obj.ste2pos_transform)
+                error('need to calibrate real space to stereotaxic coords');
             end
+            
+            pos = bsxfun(@plus,ste*obj.ste2pos_transform.rotation',obj.ste2pos_transform.offset);
+        end
+        
+        function ste=pos2ste(obj,pos)
+            %pos is [nx2]
+            if isempty(obj.pos2ste_transform)
+                error('need to calibrate real space to stereotaxic coords');
+            end
+            
+            ste = bsxfun(@plus,pos*obj.pos2ste_transform.rotation',obj.pos2ste_transform.offset);
+        end
+        
+        function timercallback(obj)
+            img = obj.getFrame;
+            image(img,'Parent',obj.vidAx);
+            if obj.isCapturing == 1
+                set(get(obj.vidAx,'parent'),'color','g');
+                title(obj.vidAx,{['update rate: ' num2str(1/obj.vidTimer.AveragePeriod)],...
+                                 ['gain: ' num2str(obj.getGain)],...
+                                 ['exposure: ' num2str(obj.getExposure)]});
+            else
+                set(get(obj.vidAx,'parent'),'color','r')
+            end
+            
+%             %if calibration done, replace pixel space axes with real space
+%             %axes
+%             if ~isempty(obj.pix2pos_transform)
+%                 
+%             end
+            
+%             set(obj.vidAx,'xtick','','ytick','','box','off','xcolor','w','ycolor','w');
         end
         
         function delete(obj)
-            obj.camObj.Acquisition.Stop;
+            stop(obj.vidTimer);
+            obj.stop;
             obj.camObj.Exit;
         end
         
