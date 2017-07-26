227b7ac1029830b81225847fc46502578668039e
Peter Zatka-Haas <p.zatka@ucl.ac.uk>
2017-07-26 13:21:24 +0100

    several incremental changes

--- GalvoController.m
+++ GalvoController.m
@@ -33,7 +33,7 @@ classdef GalvoController < handle
             obj.daqSession.outputSingleScan(v);
         end
         
-        function calibPOS2VOLTAGE(obj,ThorCam)
+        function calibPOS2VOLTAGEWith(obj,ThorCam)
             %Requires a ThorCam object for identifying the laser positions
             
             %Use grid paper marking mm
@@ -68,6 +68,67 @@ classdef GalvoController < handle
             mfiledir = fileparts(mfilename('fullpath'));
             filename = fullfile(mfiledir,'calib','calib_POS-VOLT.mat');
             save(filename,'pos2volt_transform');
+        end
+        
+        function calibPOS2VOLTAGEWithMouse(obj,ThorCam, Laser)
+            V_in = -1:1:1;
+            [Vy,Vx] = meshgrid(V_in);
+            Vx = Vx(:);
+            Vy = Vy(:);
+            c = [Vx Vy];
+            Vx(all(c,2))=[];
+            Vy(all(c,2))=[];
+            
+            
+            
+        
+            %Go through each voltage, issue to galvo, and determine the
+            %real position of that laser dot
+            obj.moveNow([0 0]); pause(0.1);
+            
+            pos = [];
+            ThorCam.toggleCamera(25);
+            for p = 1:length(Vx)
+                %Issue voltage to Galvo
+                obj.moveNow([Vx(p) Vy(p)]);
+                
+                %Use camera to determine the real position of the laser
+                %dot. Camera must be calibrated already
+                Laser.daqSession.outputSingleScan(2.5);
+                pause(1); %allow time for laser to move and new image to enter camera memory
+                pos(p,:) = ThorCam.getStimPos('manual', Laser);
+            end
+            
+            [~,~,obj.pos2volt_transform] = procrustes([Vx Vy],pos);
+            obj.pos2volt_transform.c = mean(obj.pos2volt_transform.c,1);
+            
+            pos2volt_transform = obj.pos2volt_transform;
+            
+            %%
+            testPos = 4*rand(3,2)-2;
+            for p = 1:3
+                ThorCam.vidHighlight = [testPos(p,:), 1];
+                v = obj.pos2v(testPos(p,:));
+                obj.moveNow(v);
+                Laser.daqSession.outputSingleScan(1.5);
+                %Use camera to determine the real position of the laser
+                %dot. Camera must be calibrated already
+                pause(0.5); %allow time for laser to move and new image to enter camera memory
+                pixH = ThorCam.pos2pix(testPos(p,:));
+                img = ThorCam.getFrame;
+                Laser.daqSession.outputSingleScan(0);
+                figure; image(img); axis equal;
+                hold on;
+                plot(pixH(1), pixH(2), '+r');
+                colormap gray;
+            end
+            ThorCam.vidHighlight = [0 0];
+            ThorCam.toggleCamera(100)
+            %%
+            mfiledir = fileparts(mfilename('fullpath'));
+            filename = fullfile(mfiledir,'calib','calib_POS-VOLT.mat');
+            save(filename,'pos2volt_transform');
+            pause(1);
         end
         
         function v = generateWaveform(obj,frequency,volts,totalTime)
--- laserGalvoExpt_callback.m
+++ laserGalvoExpt_callback.m
@@ -6,8 +6,6 @@ function laserGalvoExpt_callback(eventObj, LGObj)
 
 %TODO:
 %{
-1) PIP: TTL pulse to be delivered at start of stimulus
-
 2) ME: I should allow for different arrival times of variables
 
 4) Figure out how to specify laser power
@@ -17,18 +15,31 @@ function laserGalvoExpt_callback(eventObj, LGObj)
 %}
 
 if iscell(eventObj.Data) && strcmp(eventObj.Data{2}, 'experimentInit') %Experiment started
-    disp('STARTED EXPERIMENT');
-
+    expRef = eventObj.Ref;
+<<<<<<< HEAD
+    disp(['Starting Experiment: ' expRef]);
     %START LOG FILE
+    LGObj.log = []; LGObj.filepath = [];
+    LGObj.filepath = [dat.expPath(expRef, 'expInfo', 'm') '\' expRef '_galvoLog.mat'];
+    
+    LGObj.galvoCoords = [];
+=======
+    disp(['STARTED EXPERIMENT: ' expRef]);
+    %START LOG FILE
+    LGObj.log = []; LGObj.filepath = [];
+    LGObj.filepath = [dat.expPath(expRef, 'expInfo', 'm') '\' expRef '_galvoLog.mat'];
+>>>>>>> 1e44d8c63d3ce6453c296ddf30678fd5fe46fe70
     
     %Start galvo rates
-    LGObj.galvo.daqSession.Rate = 30e3;
-    LGObj.laser.daqSession.Rate = 30e3;
+    LGObj.galvo.daqSession.Rate = 10e3;
+    LGObj.laser.daqSession.Rate = 10e3;
     
     %Register triggers
     LGObj.galvo.registerTrigger([LGObj.galvoDevice '/PFI0']);
     LGObj.laser.registerTrigger([LGObj.laserDevice '/PFI0']);
 
+    %Reduce camera update rate to 1.5Hz
+    LGObj.thorcam.setUpdateRate(1.5);
 % elseif isstruct(eventObj.Data) && ~any(strcmp({eventObj.Data.name},'events.buildWaveform')) 
 %     %If any variables in the eventObj field, update them
 %     names = {eventObj.Data.name};
@@ -56,9 +67,14 @@ if iscell(eventObj.Data) && strcmp(eventObj.Data{2}, 'experimentInit') %Experime
 %     %unnecesarrily overwrite fields
     
 elseif isstruct(eventObj.Data) && any(strcmp({eventObj.Data.name},'events.newTrial'))
+<<<<<<< HEAD
+=======
     LGObj.stop;
+>>>>>>> 1e44d8c63d3ce6453c296ddf30678fd5fe46fe70
+    LGObj.thorcam.vidHighlight = [];
     
-    tic;
+    tic;    
+    allT = tic;
     
     names = {eventObj.Data.name};
     values = {eventObj.Data.value};
@@ -69,13 +85,30 @@ elseif isstruct(eventObj.Data) && any(strcmp({eventObj.Data.name},'events.newTri
             LGObj.thorcam.vidCustomCoords = LGObj.galvoCoords; %Send down to thorcam object for plotting
         end
         trialNum = values{strcmp(names,'events.trialNum')};
-        laserType = values{strcmp(names,'events.laserType')}; % 1=off, 2=illuminate 1 site, 3=illuminate 2 sites
+        laserType = values{strcmp(names,'events.laserType')}; % 0=off, 1=illuminate 1 site, 2=illuminate 2 sites
         galvoPos = values{strcmp(names,'events.galvoPos')}; %Coordinate (+ right hemisphere, - left hemisphere)
         galvoType = values{strcmp(names,'events.galvoType')}; %1 = single scan mode, 2 = multi scan mode
         laserPower = values{strcmp(names,'events.laserPower')};
-    catch
-        error('Some necessary signals werent in the events structure. Need to implement separate buildWaveform step');
+        laserDuration = values{strcmp(names,'events.laserDuration')};
+        repeatNum = values{strcmp(names,'events.repeatNum')};
+        if repeatNum >= 10
+            LGObj.thorcam.vidColor = [0.9 0 0];
+        elseif repeatNum >= 5
+            LGObj.thorcam.vidColor = [1 1 0];
+        else
+            LGObj.thorcam.vidColor = [0 1 0];
+        end
+    catch me
+        error('Problem loading trial information from eventObj');
     end
+    
+    ROW = struct;
+    ROW.delay_readVar = toc;
+<<<<<<< HEAD
+    tic;
+    
+    fprintf(['%03d) '], trialNum);
+=======
     
     disp('--');
     disp(['trialNum: ' num2str(trialNum)]);
@@ -83,38 +116,84 @@ elseif isstruct(eventObj.Data) && any(strcmp({eventObj.Data.name},'events.newTri
 %     disp(['laserType: ' num2str(laserType)]);
 %     disp(['galvoPos: ' num2str(galvoPos)]);
 
-    LGObj.galvo.daqSession.Rate = 30e3;
-    LGObj.laser.daqSession.Rate = 30e3;
-    
+>>>>>>> 1e44d8c63d3ce6453c296ddf30678fd5fe46fe70
     stereotaxicCoords = LGObj.coordID2ste(LGObj.galvoCoords, galvoPos);
-%     hemisphere = sign(galvoPos);
-%     ste = LGObj.galvoCoords(abs(galvoPos),:);
-%     
-%     if hemisphere == -1
-%         ste(1) = -ste(1);
-%     end
-%     stereotaxicCoords = ste;
     
     %Setup waveforms depending on the trial configurations
     if galvoType == 1 %UNILATERAL SINGLE SCAN MODE
-%         disp('single scan mode');
 
         %Just place the galvo at the location now, and
         %then trigger the laser output by the TTL pulse
         %(if laser should be on)
-        disp(['stereoTaxic: ' num2str(stereotaxicCoords(1)) ' ' num2str(stereotaxicCoords(2))]);
-        
+
         pos = LGObj.thorcam.ste2pos(stereotaxicCoords);
         volt = LGObj.galvo.pos2v(pos);
+        
+        ROW.delay_getCoords = toc;
+<<<<<<< HEAD
+        tic;
+=======
+>>>>>>> 1e44d8c63d3ce6453c296ddf30678fd5fe46fe70
+        
         LGObj.galvo.moveNow(volt);
         
-        if laserType>1 %If laser ON
+        ROW.delay_moveGalvo = toc;
+<<<<<<< HEAD
+        tic;
+        
+        ROW.delay_preallocLaserWaveform = NaN;
+        ROW.delay_issueLaser = NaN;
+        ROW.delay_vidHighlight = NaN;
+=======
+>>>>>>> 1e44d8c63d3ce6453c296ddf30678fd5fe46fe70
+        
+        if laserType>0 %If laser ON
             laserFrequency = 40;
             laserVolt = laserPower;
+<<<<<<< HEAD
+            laserV = LGObj.laser.generateWaveform('truncatedCos',laserFrequency,laserVolt,laserDuration,[]);
+            
+            ROW.delay_preallocLaserWaveform = toc;
+            tic;
+            
+
+            try
+%                 disp(['laser ONSET DELAY: ' num2str(laserOnsetTime)]);
+            catch
+            end
+            
+%             disp(['    delay_preallocLaserWaveform: ' num2str(ROW.delay_preallocLaserWaveform)]);
+            
+            LGObj.laser.issueWaveform(laserV);
+            fprintf(['<strong>%+0.1fML %+0.1fAP </strong>'], stereotaxicCoords(1), stereotaxicCoords(2));
+            fprintf(['%0.1fv '], laserPower);
+            fprintf('for %0.1fs',laserDuration);
+            
+            ROW.delay_issueLaser = toc;
+            tic;
+            
+%             disp(['    delay_issueLaser: ' num2str(ROW.delay_issueLaser)]);
+=======
             laserV = LGObj.laser.generateWaveform('truncatedCos',laserFrequency,laserVolt,10,[]);
-                    
+            
+            ROW.delay_preallocLaserWaveform = toc;
+            
             disp(['laser ON voltage=: ' num2str(laserPower)]);
             LGObj.laser.issueWaveform(laserV);
+            
+            ROW.delay_issueLaser = toc;
+>>>>>>> 1e44d8c63d3ce6453c296ddf30678fd5fe46fe70
+            
+            %Display coordinate on video feed
+            LGObj.thorcam.vidHighlight = stereotaxicCoords;
+            
+            ROW.delay_vidHighlight = toc;
+<<<<<<< HEAD
+            tic;
+            
+%             disp(['    delay_vidHighlight: ' num2str(ROW.delay_vidHighlight)]);
+=======
+>>>>>>> 1e44d8c63d3ce6453c296ddf30678fd5fe46fe70
         end
         
                 
@@ -122,30 +201,90 @@ elseif isstruct(eventObj.Data) && any(strcmp({eventObj.Data.name},'events.newTri
         stereotaxicCoords = [stereotaxicCoords; -stereotaxicCoords(1), stereotaxicCoords(2)];
         pos = LGObj.thorcam.ste2pos(stereotaxicCoords);
         
+<<<<<<< HEAD
+        ROW.delay_getCoords = toc;
+        tic;
+        ROW.delay_moveGalvo = NaN;
+        ROW.delay_preallocLaserWaveform = NaN;
+        ROW.delay_issueLaser = NaN;
+        ROW.delay_vidHighlight = NaN;
+=======
         disp(['galvo scan Stereotaxic: ' num2str(stereotaxicCoords(1,1)) ',' num2str(stereotaxicCoords(1,2)) ' & ' num2str(stereotaxicCoords(2,1)) ',' num2str(stereotaxicCoords(2,2))]);
 
-        if laserType> 1 %Laser on for ONE location (the first one in the list)
+>>>>>>> 1e44d8c63d3ce6453c296ddf30678fd5fe46fe70
+        if laserType> 0 %Laser on for ONE location (the first one in the list)
             laserVolt = laserPower;
 
-            if laserType == 2
+            if laserType == 1
+<<<<<<< HEAD
+                fprintf(['<strong>%+0.1fML %+0.1fAP</strong> <-> %+0.1fML %+0.1fAP '], stereotaxicCoords(1,1), stereotaxicCoords(1,2), stereotaxicCoords(2,1), stereotaxicCoords(2,2));
+                LGObj.scan('onesite',pos,laserDuration,laserVolt);
+                ROW.delay_issueLaser = toc;
+                tic;
+                
+            elseif laserType == 2
+                fprintf(['<strong>%+0.1fML %+0.1fAP</strong> <-> <strong>%+0.1fML %+0.1fAP</strong> '], stereotaxicCoords(1,1), stereotaxicCoords(1,2), stereotaxicCoords(2,1), stereotaxicCoords(2,2));
+
+                LGObj.scan('multisite',pos,laserDuration,laserVolt);
+                ROW.delay_issueLaser = toc;
+                tic;
+=======
                 disp(['laser ON at 1st site. volt=' num2str(laserPower)]);
                 LGObj.scan('onesite',pos,5,laserVolt);
                 
-            elseif laserType == 3
+            elseif laserType == 2
                 disp(['laser ON at both sites. volt=' num2str(laserPower)]);
                 LGObj.scan('multisite',pos,5,laserVolt);
+>>>>>>> 1e44d8c63d3ce6453c296ddf30678fd5fe46fe70
             end
+            
+            fprintf(['%0.1fv '], laserPower);
+            fprintf('for %0.1fs',laserDuration);
+
+            LGObj.thorcam.vidHighlight = stereotaxicCoords;
+            ROW.delay_vidHighlight = toc;
+            tic;
         else
-            LGObj.scan('multisite',pos,5,0);
+            LGObj.scan('multisite',pos,laserDuration,0);
+            fprintf(['%+0.1fML %+0.1fAP <-> %+0.1fML %+0.1fAP '], stereotaxicCoords(1,1), stereotaxicCoords(1,2), stereotaxicCoords(2,1), stereotaxicCoords(2,2));
+
         end
 
     
     end
+<<<<<<< HEAD
+    
+    %Save these details to a log
+=======
     toc;
+    
+    %Save these details to a log
+%     ROW = struct;
+>>>>>>> 1e44d8c63d3ce6453c296ddf30678fd5fe46fe70
+    ROW.trialNum = trialNum;
+    ROW.laserType = laserType;
+    ROW.galvoPos = galvoPos;
+    ROW.galvoType = galvoType;
+    ROW.laserPower = laserPower;
+<<<<<<< HEAD
+    ROW.tictoc = toc(allT);
+%     disp(['    total time: ' num2str(ROW.tictoc)]);
+    LGObj.appendToLog(ROW);
+    fprintf('\n');
+% 
+% elseif isstruct(eventObj.Data) && any(strcmp({eventObj.Data.name},'events.galvoAndLaserEnd'))
+%     tic
+%     LGObj.stop; %This can sometimes take a while
+%     off_time = toc;
+%     fprintf('OFF (%0.2fs)\n',off_time);
+=======
+    ROW.tictoc = toc;
+    LGObj.appendToLog(ROW);
 
 elseif isstruct(eventObj.Data) && any(strcmp({eventObj.Data.name},'events.galvoAndLaserEnd'))
     disp('GALVO AND LASER OFF');
     LGObj.stop;
+>>>>>>> 1e44d8c63d3ce6453c296ddf30678fd5fe46fe70
     
 elseif iscell(eventObj.Data) && strcmp(eventObj.Data{2}, 'experimentEnded')
     LGObj.stop;
@@ -153,6 +292,18 @@ elseif iscell(eventObj.Data) && strcmp(eventObj.Data{2}, 'experimentEnded')
     %Remove triggers
     LGObj.galvo.removeTrigger;
     LGObj.laser.removeTrigger;
+    
+    %Save log 
+    LGObj.saveLog;
+<<<<<<< HEAD
+    
+    %Return thorcam update rate to 2Hz
+    LGObj.thorcam.setUpdateRate(2);
+    LGObj.thorcam.vidColor = [1 0 0];
+    
+    fprintf('Experiment Ended.\n');
+=======
+>>>>>>> 1e44d8c63d3ce6453c296ddf30678fd5fe46fe70
 end
 
 end
